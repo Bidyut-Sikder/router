a819cbfeebf822b87fa0887b42f799a99174ecd8
bidyutsikder <bidyutsikder420@gmail.comm>
2023-11-23 11:37:17 +0600

    feat :react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Outlet,NavLink } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      
+      <nav>
+        <NavLink to={'/router/'}>Home </NavLink>
+        <NavLink to={'/router/contact/'}>Contact </NavLink>
+      </nav>
+      <h1>this is bidyut</h1>
+   
+      <Outlet />
+
+
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,32 @@ import App from './App.jsx'
 //import './index.css'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Contact from './pages/Contact.jsx';
 
-
+const router=createBrowserRouter([
+    {
+        path: '/router/',
+        element:<App />,
+        children:[
+            {
+                path: '/router/',
+                element:< Home/>
+            },
+            {
+                path: '/router/contact',
+                element:< Contact/>
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+<RouterProvider router={router} />
+
+
 
 )
  

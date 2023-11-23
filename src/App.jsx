400f990f68ba30b8abad6829942b099ddc4bfe0d
bidyutsikder <bidyutsikder420@gmail.comm>
2023-11-23 11:10:00 +0600

    add : initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import React from "react";
-
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {GetToken} from "./utility/TokenHelper";
-import ProductListPage from "./Pages/ProductListPage";
-import CartListPage from "./Pages/CartListPage";
-import UserLoginPage from "./Pages/UserLoginPage";
-import OTPPage from "./Pages/OTPPage";
-import NavBar from "./components/NavBar";
+import React from 'react';
 
 const App = () => {
-  if (GetToken()) {
-    return (
-      <>
-        <BrowserRouter>
-     
-          <Routes>
-            <Route path="/cart_project" element={<ProductListPage />} />
-            <Route path="/cart" element={<CartListPage />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <BrowserRouter>
-       
-          <Routes>
-            <Route path="/cart_project" element={<ProductListPage />} />
-
-            <Route path="/login" element={<UserLoginPage />} />
-            <Route path="/otp" element={<OTPPage />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    );
-  }
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default App;
